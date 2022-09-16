a5fdf124261751e326e4dc837aa5d4491da93564
SP <201708114@uaeu.ac.ae>
2022-09-16 10:31:34 +0400

    No change

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up up App.js to start working on your app!</Text>
+      <Text>Hey Khalil!</Text>
       <StatusBar style="auto" />
     </View>
   );
