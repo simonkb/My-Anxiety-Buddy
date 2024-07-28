ee4207187aafb8fa928423f98ba5b664318eabce
Simon K. Darota <85609748+simonkb@users.noreply.github.com>
2024-07-28 18:34:28 +0400

    add react native language detector

--- src/transilations/i18n.js
+++ src/transilations/i18n.js
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from "react-i18next";
 import en from "./en/transilation.json";
 import ar from "./ar/transilation.json";
-import LanguageDetector from "i18next-browser-languagedetector";
+import RNLanguageDetector from '@os-team/i18next-react-native-language-detector';
 
 export const languageResources = {
   en: { translation: en },
@@ -10,16 +10,11 @@ export const languageResources = {
 };
 
 i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  // .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
+  .use(RNLanguageDetector)
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    fallbackLng: "ar",
+    fallbackLng: "en",
     interpolation: {
       escapeValue: false, 
     },
