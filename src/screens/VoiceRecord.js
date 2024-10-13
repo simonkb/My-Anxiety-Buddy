903f70225c7d9d4d4c12504e258ed542f9a2caab
Simon K. Darota <85609748+simonkb@users.noreply.github.com>
2024-10-13 14:51:44 +0400

    added background image for VoiceRecord

--- src/screens/VoiceRecord.js
+++ src/screens/VoiceRecord.js
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  ImageBackground,
+} from "react-native";
 import { Audio } from "expo-av";
 import { MaterialIcons } from "@expo/vector-icons"; // Import Material Icons
+import { useGlobalState, bg1, bg2, bg3 } from "../states/state";
 
 const VoiceRecord = () => {
   const [recording, setRecording] = useState(null);
@@ -11,6 +19,16 @@ const VoiceRecord = () => {
   const [isPlaying, setIsPlaying] = useState(false); // State for playing status
   const [notificationVisible, setNotificationVisible] = useState(false); // State for notification visibility
   const [textMessage, setTextMessage] = useState(""); // State for the text message
+
+  let defaultBg = useGlobalState("defaultBackgroundImage");
+  let currentBg;
+  if (defaultBg[0] === "bgOrange") {
+    currentBg = bg3;
+  } else if (defaultBg[0] === "bgBlue") {
+    currentBg = bg2;
+  } else {
+    currentBg = bg1;
+  }
 
   // Array of random messages
   const messages = [
@@ -119,52 +137,63 @@ const VoiceRecord = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.instruction}>
-        Please read the following text while recording. When you are done, click
-        Stop.
-      </Text>
-      <View style={styles.textContainer}>
-        <Text style={styles.paragraph}>
-          {textMessage} {/* Display the random message */}
-        </Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        {isRecording ? (
-          <>
-            <TouchableOpacity style={styles.button} onPress={stopRecording}>
-              <MaterialIcons name="stop" size={30} color="#fff" />
-              <Text style={styles.buttonText}>Stop Recording</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={discardRecording}>
-              <MaterialIcons name="delete" size={30} color="#fff" />
-              <Text style={styles.buttonText}>Discard Recording</Text>
-            </TouchableOpacity>
-          </>
-        ) : (
-          <TouchableOpacity style={styles.button} onPress={startRecording}>
-            <MaterialIcons name="mic" size={30} color="#fff" />
-            <Text style={styles.buttonText}>Start Recording</Text>
-          </TouchableOpacity>
-        )}
-      </View>
-      {recordedUri && !isPlaying && (
-        <TouchableOpacity style={styles.button} onPress={playSound}>
-          <MaterialIcons name="play-arrow" size={30} color="#fff" />
-          <Text style={styles.buttonText}>Play Recording</Text>
-        </TouchableOpacity>
-      )}
-      {recordedUri && isPlaying && (
-        <Text style={styles.recordingStatus}>Playing...</Text>
-      )}
-      {notificationVisible && (
-        <View style={styles.notification}>
-          <Text style={styles.notificationText}>
-            Recording saved: {recordedUri}
+    <ImageBackground
+      source={currentBg}
+      resizeMode="cover"
+      style={styles.bgImage}
+    >
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.instruction}>
+            Please read the following text while recording. When you are done,
+            click Stop.
           </Text>
         </View>
-      )}
-    </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.paragraph}>
+            {textMessage} {/* Display the random message */}
+          </Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          {isRecording ? (
+            <>
+              <TouchableOpacity style={styles.button} onPress={stopRecording}>
+                <MaterialIcons name="stop" size={30} color="#fff" />
+                <Text style={styles.buttonText}>Stop Recording</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={discardRecording}
+              >
+                <MaterialIcons name="delete" size={30} color="#fff" />
+                <Text style={styles.buttonText}>Discard Recording</Text>
+              </TouchableOpacity>
+            </>
+          ) : (
+            <TouchableOpacity style={styles.button} onPress={startRecording}>
+              <MaterialIcons name="mic" size={30} color="#fff" />
+              <Text style={styles.buttonText}>Start Recording</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+        {recordedUri && !isPlaying && (
+          <TouchableOpacity style={styles.button} onPress={playSound}>
+            <MaterialIcons name="play-arrow" size={30} color="#fff" />
+            <Text style={styles.buttonText}>Play Recording</Text>
+          </TouchableOpacity>
+        )}
+        {recordedUri && isPlaying && (
+          <Text style={styles.recordingStatus}>Playing...</Text>
+        )}
+        {notificationVisible && (
+          <View style={styles.notification}>
+            <Text style={styles.notificationText}>
+              Recording saved: {recordedUri}
+            </Text>
+          </View>
+        )}
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -174,13 +203,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
-    backgroundColor: "#f0f4f8", // Light background color
   },
   instruction: {
     marginBottom: 20,
     fontSize: 16,
     textAlign: "center",
     color: "#333", // Dark text color
+    fontWeight: "bold",
   },
   textContainer: {
     backgroundColor: "#fff", // White background for the text container
@@ -247,6 +276,9 @@ const styles = StyleSheet.create({
     marginTop: 10,
     fontSize: 14,
     color: "green",
+  },
+  bgImage: {
+    flex: 1,
   },
 });
 
